5fb8e1a9fee221d2ceb526e755844e1c251adf1a
yipengmaxzhang <maxzhang2728@gmail.com>
2025-02-27 20:39:23 +0000

    added all 4 lab codes

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,54 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    result = ""
+    n = 8
+    while n == n:
+        result += "*" * n
+        result += "\n"
+        for i in range(n - 2):
+            result += "*" + " " * (n - 2) + "*"
+            result += "\n"
+        if n == 1:
+            return result.strip()
+            break
+        result += "*" * n
+        return result.strip()
+        if n == n:
+            break
+
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ""
+    n = 5
+    row = 1
+    numbers = 1
+    while row <= n:
+        while numbers <= row:
+            result += str(n - (n - numbers))
+            numbers += 1
+            result += "\n"
+            numbers = 1
+            row += 1
+        return result.strip()
+        if n == n:
+            break
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    result = ""
+    n = 8
+    count = 0
+    sum = 0
+    while count < n:
+        sum += (n - count)
+        count += 1
+    result += str(sum)
+    return result
 
 # Example for n = 4:
 #    *
@@ -24,4 +60,15 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    result = ""
+    n = 4
+    row = 1
+    i = 1
+    while row <= n:
+        result += " " * (n - row) + "*" * i + " " * (n - row)
+        if row == n
+            break
+        result += "\n"
+        row += 1
+        i += 2
+    return result
